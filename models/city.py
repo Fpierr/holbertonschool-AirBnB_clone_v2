36486d2ce6233fbbea2b4702afa4449fa48728ec
Fpierr <fanuelp9@gmail.com>
2024-03-28 11:26:11 +0100

    6

--- models/city.py
+++ models/city.py
@@ -5,13 +5,12 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey(
-        'states.id', ondelete="CASCADE"), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id', ondelete="CASCADE"),
+                      nullable=False)
 
     places = relationship("Place", backref="cities", cascade="all, delete",
                           passive_deletes=True)
--- models/state.py
+++ models/state.py
@@ -1,26 +1,25 @@
+
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+"""state class inherit in base and base model"""
 from models.base_model import BaseModel, Base
 from models.city import City
 from models import storage
 from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state",
-                cascade="all, delete", passive_deletes=True)
+        cities = relationship("City", backref="state", cascade="all, delete",
+                              passive_deletes=True)
     else:
         @property
         def cities(self):
-            """Getter attribute that returns the list of City instances
-            with state_id equals to the current State.id"""
+            """returns the list of City instances"""
             new_list = []
             all_cities = storage.all(City)
             for element in all_cities.values():
