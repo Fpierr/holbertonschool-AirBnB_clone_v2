e446b426d0ea0540cf9da663c5a97cf3f238f873
Fpierr <fanuelp9@gmail.com>
2024-03-28 11:04:03 +0100

    6

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base
 
 
+
 class DBStorage:
     """DBStorage class"""
 
@@ -16,11 +17,11 @@ class DBStorage:
     def __init__(self):
         """Initialize DBStorage"""
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(getenv('HBNB_MYSQL_USER'),
-                                              getenv('HBNB_MYSQL_PWD'),
-                                              getenv('HBNB_MYSQL_HOST'),
-                                              getenv('HBNB_MYSQL_DB')),
-                                      pool_pre_ping=True)
+                .format(getenv('HBNB_MYSQL_USER'),
+                    getenv('HBNB_MYSQL_PWD'),
+                    getenv('HBNB_MYSQL_HOST'),
+                    getenv('HBNB_MYSQL_DB')),
+                pool_pre_ping=True)
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
@@ -68,3 +69,5 @@ class DBStorage:
                                         expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    storage = DBStorage()
