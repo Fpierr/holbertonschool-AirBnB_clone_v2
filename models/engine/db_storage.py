06a6f3462263489ee5c8dff2ed4f0a525bd618aa
Fpierr <fanuelp9@gmail.com>
2024-03-29 15:32:34 +0100

    update user

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -10,7 +10,7 @@ from models.city import City
 from models.place import Place
 from models.review import Review
 from models.amenity import Amenity
-
+from models.base_model import Base
 
 class DBStorage:
     """DBStorage class"""
