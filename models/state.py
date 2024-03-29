373d6ba129a603d4d641d1ef34813c042de1f50b
Fpierr <fanuelp9@gmail.com>
2024-03-29 20:34:48 +0100

    state

--- models/state.py
+++ models/state.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-
-import os
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-
-    name = Column(String(128), nullable=False)
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", back_populates="state",
-                              cascade="all, delete-orphan")
+    name = ""
