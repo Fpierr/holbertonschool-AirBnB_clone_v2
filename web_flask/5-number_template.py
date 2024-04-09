ecdb7f55fec558b3a29ff1bbe5f2186a68e87816
Fpierr <fanuelp9@gmail.com>
2024-04-09 13:22:38 +0200

    templates number

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
-"""Starts a Flask web application."""
+"""Start a Flask web application."""
 
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
+    """Display 'Hello HBNB!' when accessing root URL."""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """Displays 'HBNB'"""
+def hbnb():
+    """Display 'HBNB' when accessing '/hbnb' URL."""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def display_c(text):
-    """Displays 'C ', followed by the value of the text variable"""
+def display_c_text(text):
+    """Displays 'C ' followed by the value of text"""
     return 'C {}'.format(text.replace('_', ' '))
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def display_python(text):
-    """Displays 'Python ', followed by the value of the text variable"""
+def display_python_text(text):
+    """Displays 'Python ' followed by the value of text"""
     return 'Python {}'.format(text.replace('_', ' '))
 
 
