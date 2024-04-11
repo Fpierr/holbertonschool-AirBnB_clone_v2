cb0dbfc3227b01eefff945082d2dcb77ced9f7dc
Fpierr <fanuelp9@gmail.com>
2024-04-11 15:32:33 +0200

    8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-""" Script to start a Flask web application """
+"""Start a web app"""
 
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
 
 
+@app.route('/cities_by_states')
+def cities_by_states():
+    """render cities by states sorted by name"""
+    path = '8-cities_by_states.html'
+    states = storage.all(State)
+    return render_template(path, states=states)
+
+
 @app.teardown_appcontext
-def shutdown(exception):
-    """ Close the storage after each request """
+def app_teardown(arg=None):
+    """Clean-up session"""
     storage.close()
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """ Display a list of states with their cities """
-    all_states = storage.all(State)
-    return render_template('8-cities_by_states.html', all_states=all_states)
-
-
 if __name__ == '__main__':
-    """ Main """
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
